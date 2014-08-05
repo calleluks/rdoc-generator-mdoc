af4f0751daf4a063390f640e9bd9c4aaf4da8526
Calle Erlandsson <calle@thoughtbot.com>
2014-08-05 13:33:04 +0200

    Accept files as command line arguments.

--- lib/rdoc2mdoc/cli.rb
+++ lib/rdoc2mdoc/cli.rb
@@ -9,7 +9,7 @@ module Rdoc2mdoc
     # Run the rdoc2mdoc program using Rdoc2mdoc::Generator.
     def run(argv)
       options = RDoc::Options.new
-      options.files = [""]
+      options.files = argv
       options.setup_generator "rdoc2mdoc::generator"
       RDoc::RDoc.new.document options
     end
